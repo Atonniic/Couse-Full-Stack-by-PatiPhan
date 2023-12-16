618df9d91280dc278fb5a25fb9372bf799c92008
mkksdx2 <chaipot.k@ku.th>
2023-12-17 00:39:37 +0700

    Clip 08/29

--- nodejs-blog/index.js
+++ nodejs-blog/index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const ejs = require('ejs')
+const mongoose = require('mongoose')
+const BlogPost = require('./models/BlogPost')
+
+mongoose.connect('mongodb+srv://admin:1234@cluster0.ebikiov.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+})
 
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
@@ -24,6 +32,22 @@ app.get('/contact', (req, res) => {
 app.get('/post', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'pages/post.html'))
     res.render('post')
+})
+
+app.get('/posts/new', (req, res) => {
+    res.render('create')
+})
+
+app.post('/posts/store', (req, res) => {
+    // console.log(req.body)
+    // res.redirect('/')
+
+    BlogPost.create(req.body).then(() => {
+        console.log('Data inserted successfully')
+        res.redirect('/')
+    }).catch(err => {
+        console.log(err)
+    })
 })
 
 app.listen(4000, () => {
---
+++ nodejs-blog/test.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const BlogPost = require('./models/BlogPost')
+
+mongoose.connect('mongodb+srv://admin:1234@cluster0.ebikiov.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+})
+
+// Insert data
+// BlogPost.create({   
+//     title: 'This is first post',
+//     body: 'This is first post content'
+// }).then(() => {
+//     console.log('Insert data successfully')
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+// Read data
+let id = "657c87ed1b50f713045278d5"
+
+// BlogPost.findById(id).then((data) => {
+//     console.log('Data was found')
+//     console.log(data)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+// Update data
+// BlogPost.findByIdAndUpdate(id, {   
+//     body: 'Button will drive the Jota Porsche 963 in the top hypercar class in the World Endurance Championship, which includes the famous 24 Hours of Le Mans race.'
+// }).then(() => {
+//     console.log('Update data successfully')
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+// Delete data
+BlogPost.findByIdAndDelete(id).then(() => {
+    console.log('Deleted data successfully')
+}).catch((err) => {
+    console.log(err)
+})
